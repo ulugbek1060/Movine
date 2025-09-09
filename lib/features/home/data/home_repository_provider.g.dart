4dc43b75ef5981106d4a4bcea976085170f7d66d
ulugbek1060 <ulugg1060@gmail.com>
2025-09-10 00:38:12 +0500

    Fixed the provider structure: Created a single base provider that takes MovieType as an argument
    Added proper individual providers: Used FutureProvider.autoDispose wrappers for each movie type
    
    Improved error handling: Better state management during loading and error states
    
    Added UI integration: Show how to fetch data in the UI using ref.watch() and ref.read()
    
    Implemented proper refresh: Added refresh functionality that works correctly
    
    Added load more functionality: Buttons to fetch next pages

---
+++ lib/features/home/data/home_repository_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'home_repository_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$homeRepositoryHash() => r'5cf44bdc7c545bd34310e7335289ec9b9b4e8ebd';
+
+/// See also [homeRepository].
+@ProviderFor(homeRepository)
+final homeRepositoryProvider = AutoDisposeProvider<IHomeRepository>.internal(
+  homeRepository,
+  name: r'homeRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$homeRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef HomeRepositoryRef = AutoDisposeProviderRef<IHomeRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
